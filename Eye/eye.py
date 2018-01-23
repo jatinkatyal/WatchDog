899fcf6b5e6a3b23cf4e93fdb1e8a0c6f98cf2e5
Jatin Katyal <jatinkatyal96@gmail.com>
2018-01-23 17:48:58 +0530

    Eye can use images on system also.

--- Eye/eye.py
+++ Eye/eye.py
@@ -1,24 +1,30 @@
 import cv2
 
-class Eye(object):
-	"""Eyes sees everything"""
-	def __init__(self):
+class Eye:
+	"""Eyes sees everything, captures form camera by default if scene is not provided."""
+	def __init__(self,path=None):
+		self.path = path
 		self.cam = cv2.VideoCapture(0)
 
 	def see(self):
-		ret,frame = self.cam.read()
-		try:
-			if ret:
-				cv2.waitKey(70)
-				return frame
-		except:
-			print("Error reading image in eye")
+		if self.path:
+			try:
+				frame = cv2.imread(self.path)
+			except:
+				print('Error reading file through eye.')
+		else:
+			try:
+				ret,frame = self.cam.read()
+			except:
+				print("Error capturing image in eye")
+		cv2.waitKey(50)
+		return frame
 
 	def closeEye(self):
 		self.cam.release()
 
 if __name__=='__main__':
-	cam1 = Eye()
+	cam1 = Eye('test.jpg')
 	for i in range(30):
 		cv2.imshow('img',cam1.see())
 	cam1.closeEye()
