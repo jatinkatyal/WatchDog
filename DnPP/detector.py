abe8d497150c35f7e1d4b05347d11ca6c3b0d85d
Jatin Katyal <jatinkatyal96@gmail.com>
2018-03-30 22:23:36 +0530

    Modified detector code.

--- DnPP/detector.py
+++ DnPP/detector.py
@@ -9,20 +9,21 @@ class Detector:
 		self.rightEyeDetector = cv2.CascadeClassifier(d+'/haarcascade_righteye_2splits.xml')
 
 	def detect(self,frame):
-		img = PP.detectorPP(frame)
+		img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		img=cv2.equalizeHist(img)
 		faceCoords = self.faceDetector.detectMultiScale(img,scaleFactor=1.1,minNeighbors=5)
 		faces =[]
 		for x,y,w,h in faceCoords:
-			face = frame[y:y+h,x:x+w]
+			face = img[y:y+h,x:x+w]
 			if self.detectEyes(face):
-				faces.append(PP.rescale(face))
+				faces.append(cv2.resize(face,(150,150)))
 		if len(faces)<1:
 			return None
 		return faces
 	
 	def detectEyes(self,face):
-		leftEye = self.leftEyeDetector.detectMultiScale(PP.toGray(face),scaleFactor=1.1,minNeighbors=5)
-		rightEye = self.rightEyeDetector.detectMultiScale(PP.toGray(face),scaleFactor=1.1,minNeighbors=5)
+		leftEye = self.leftEyeDetector.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5)
+		rightEye = self.rightEyeDetector.detectMultiScale(face,scaleFactor=1.1,minNeighbors=5)
 		if len(leftEye)>0 and len(rightEye)>0:
 			return True
 		return False
